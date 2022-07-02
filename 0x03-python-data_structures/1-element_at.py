9393f797059a016a55c3ae37b900a4f5e050df6c
Mboko David <98537263+Meekdavid@users.noreply.github.com>
2022-07-01 20:55:17 -0700

    Datas

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def element_at(my_list, inx):
+    
+    if 0 <= inx < len(my_list):
+        
+        return my_list[inx]
---
+++ 0x03-python-data_structures/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from variable_load_5 import a
+
+value = a
+
+print(value)
+
+__name__ == "__main__"
